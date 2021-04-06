e5bfda4ed1b741d78278e16af1b952d280a949ee
EsitoneX <phoom0529@gmail.com>
2021-04-06 17:31:32 +0700

    ➕🎨 Add redux-logger & Create store[folder]

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import reduxThunk from 'redux-thunk';
-import { persistReducer, persistStore } from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/es/integration/react';
-import storage from 'redux-persist/lib/storage';
 
 import './css/tailwind.css';
 
 import App from './App';
-import rootReducer from './reducers';
 
-const persistedReducer = persistReducer({ key: 'root', storage }, rootReducer);
-const store = createStore(persistedReducer, applyMiddleware(reduxThunk));
-const persistor = persistStore(store);
+import configureStore from './store/configureStore';
+
+const persistor = persistStore(configureStore);
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
+        <Provider store={configureStore}>
             <PersistGate loading={null} persistor={persistor}>
                 <App />
             </PersistGate>
